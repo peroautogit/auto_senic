469f3ee361cfa952f2bcf3e30af83cf1763f50ae
ninichuuh <105721940+ninichuuh@users.noreply.github.com>
2022-07-10 19:01:30 +0200

    lelll

--- src/main.js
+++ src/main.js
@@ -81,7 +81,7 @@ const runPuppeteer = async (url) => {
   const dom = new jsdom.JSDOM(htmlString);
 
   console.log("parsing njuskalo.hr data");
-  const result = dom.window.document.querySelectorAll(".EntityList-items"); // do tuda radi kako treba, neznam sta tu treeba hvatat
+  const result = dom.window.document.querySelectorAll(".entity-title"); // do tuda radi kako treba, neznam sta tu treeba hvatat
   console.log(result);
   for (const element of result) {
     const urlPath = element?.querySelectorAll("a")?.[0]?.href; //i ovo
