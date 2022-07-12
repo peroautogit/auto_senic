109989c2df7ac4853ce20f2816d45f0ecef01c70
ninouii <105721940+ninichuuh@users.noreply.github.com>
2022-07-12 13:46:47 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -81,7 +81,7 @@ const runPuppeteer = async (url) => {
   const dom = new jsdom.JSDOM(htmlString);
 
   console.log("parsing njuskalo.hr data");
-  const result = dom.window.document.querySelectorAll(".EntityList.EntityList--Standard.EntityList--Regular.EntityList--ListItemRegularAd"); // do tuda radi kako treba, neznam sta tu treeba hvatat
+  const result = dom.window.document.querySelectorAll("#form_browse_detailed_search > div > div.content-main > div.block-standard.block-standard--epsilon > div.EntityList.EntityList--Standard.EntityList--Regular.EntityList--ListItemRegularAd.EntityList--itemCount_35738 > ul"); // do tuda radi kako treba, neznam sta tu treeba hvatat
   console.log(result);
   for (const element of result) {
     const urlPath = element?.querySelectorAll("a")?.[0]?.href; 
