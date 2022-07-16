40704cbae94ad7080f7c9296f85855d17c02827f
ninouii <105721940+ninichuuh@users.noreply.github.com>
2022-07-16 15:27:10 +0200

    #

--- src/main.js
+++ src/main.js
@@ -84,8 +84,10 @@ const runPuppeteer = async (url) => {
 
 
   console.log("parsing njuskalo.hr data");
-  const result = document.querySelector("#form_browse_detailed_search > div > div.content-main > div.block-standard.block-standard--epsilon > div.EntityList.EntityList--Standard.EntityList--Regular.EntityList--ListItemRegularAd > ul"); // do tuda radi kako treba, neznam sta tu treeba hvatat
+  const result =document.querySelectorAll(".block-standard.block-standard--epsilon"); // do tuda radi kako treba, neznam sta tu treeba hvatat
   console.log({result});
+  
+  
   for (const element of result) {
     const urlPath = element?.querySelectorAll("a")?.[0]?.href; 
     console.log({urlPath});
